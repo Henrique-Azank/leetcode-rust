83fe8a762395c334a2309070e2cd7a14f3e7a005
Henrique <henriqueazank@gmail.com>
2025-06-16 15:22:07 -0300

    feat: naive version - median two sorted arrays

--- src/hard/median_two_sorted_arrays.rs
+++ src/hard/median_two_sorted_arrays.rs
@@ -15,4 +15,23 @@
 pub fn naive_find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     // Merge the two arrays
     let mut merged: Vec<i32> = [nums1, nums2].concat();
+
+    // Sort the merged array
+    merged.sort();
+
+    // Get the size of the vector
+    let size: i32 = merged.len() as i32;
+
+    // If the size is even, return the average of the two middle elements
+    if size % 2 == 0 {
+        // Find the two middle elements
+        let mid1: i32 = merged[(size / 2 - 1) as usize];
+        let mid2: i32 = merged[(size / 2) as usize];
+
+        // Return the average of the two middle elements
+        return (mid1 + mid2) as f64 / 2.0;
+    }
+
+    // If the size is odd, return the middle element
+    return merged[(size / 2) as usize] as f64;
 }
