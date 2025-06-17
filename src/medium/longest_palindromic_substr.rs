92565a82565f80b3396c8bff3089ffc3ccb2e311
Henrique <henriqueazank@gmail.com>
2025-06-17 07:29:34 -0300

    feat: longest palindromic substring - initial dynamic solution (2D memoized array)

--- src/medium/longest_palindromic_substr.rs
+++ src/medium/longest_palindromic_substr.rs
@@ -70,6 +70,70 @@ pub fn brute_longest_palindrome(s: String) -> String {
     is also a palindrome.
 */
 pub fn dynamic_longest_palindrome(s: String) -> String {
-    println!("{s}");
-    return "".to_string();
+    // Instantiate the index and lenght of the longest palindromic substring
+    let mut start_index: usize = 0;
+    let mut max_length: usize = 1;
+
+    /*
+        Instantiate the 2D vector to store the results of the palindromic checks.
+        Since the max value of the string length is 1000, we can use a 2D vector
+        of size 1000x1000 to store the results.
+    */
+    let mut memoized_subs: [[bool; 1000]; 1000] = [[false; 1000]; 1000];
+
+    // Size of the string
+    let string_len: usize = s.len();
+
+    /*
+        Mark all the single characters substrings as palindromes
+        (i.e max_lenght = 1)
+    */
+    for i in 0..string_len {
+        memoized_subs[i][i] = true;
+    }
+
+    /*
+        Check for the substrings with length 2
+        If the characters are equal, mark the substring as a palindrome
+    */
+    for i in 0..string_len - 1 {
+        // If the current character is equal to the next character
+        if s.as_bytes()[i] == s.as_bytes()[i + 1] {
+            // Mark the substring as a palindrome
+            memoized_subs[i][i + 1] = true;
+
+            // Update the start index and max length
+            start_index = i;
+            max_length = 2;
+        }
+    }
+
+    /*
+        Check for the substrings with length greater than 2
+        If the characters at the start and end of the substring are equal,
+        and the substring between them is a palindrome, mark the substring as a palindrome.
+    */
+    for length in 3..=string_len {
+        // Limit for the search
+        let search_limit: usize = string_len - length + 1;
+
+        for i in 0..search_limit {
+            // Calculate the end index of the substring
+            let j: usize = i + length - 1;
+
+            // Check if the characters at the start and end of the substring are equal
+            // and if the substring between them is a palindrome
+            if s.as_bytes()[i] == s.as_bytes()[j] && memoized_subs[i + 1][j - 1] {
+                // Mark the substring as a palindrome
+                memoized_subs[i][j] = true;
+
+                // Update the start index and max length
+                start_index = i;
+                max_length = length;
+            }
+        }
+    }
+
+    // Return the longest palindromic substring
+    return s[start_index..start_index + max_length].to_string();
 }
